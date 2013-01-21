d76407f775e028d26c2b4afd154170287952027a
Steve Kemp <steve@steve.org.uk>
2013-01-21 19:40:47 +0000

    perltidy.
      Ran the code through perltidy.

--- submit.pl
+++ submit.pl
@@ -44,14 +44,14 @@ use warnings;
 
 use IO::Socket;
 
-my $msg = shift( @ARGV );
-exit(0) unless( defined( $msg ) && length($msg) );
+my $msg = shift(@ARGV);
+exit(0) unless ( defined($msg) && length($msg) );
 
 my $sock = IO::Socket::INET->new( Proto    => 'udp',
                                   PeerPort => 4433,
                                   PeerAddr => "localhost",
-                                ) ;
+                                );
 
-$sock->send( $msg );
+$sock->send($msg);
 exit(0);
 
